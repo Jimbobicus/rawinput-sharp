aa2f95057af957900459fb5120966b2f3406575b
Jimbobicus <jdonald@muellerwp.com>
2020-06-02 16:01:15 -0700

    move event args to main assembly

---
+++ RawInput.Sharp/RawInputEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Linearstar.Windows.RawInput;
+
+namespace Linearstar.Windows.RawInput
+{
+    public sealed class RawInputEventArgs : EventArgs
+    {
+        public RawInputData Data { get; }
+        public IntPtr Param { get; }
+
+        public RawInputEventArgs() : this(null, IntPtr.Zero) {}
+        public RawInputEventArgs(RawInputData data) : this(data, IntPtr.Zero) { }
+        public RawInputEventArgs(RawInputData data, IntPtr param)
+        {
+            Data = data;
+            Param = param;
+        }
+    }
+}
